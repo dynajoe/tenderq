17bf7bf7d93e6cdfae18dc361d0abaab9336eaf9
Joe Andaverde <joe.andaverde@gmail.com>
2019-03-07 23:59:00 -0600

    Trying different layers

--- model/model.py
+++ model/model.py
@@ -1,5 +1,5 @@
 from keras.applications import MobileNet
-from keras.layers import Dense, GlobalAveragePooling2D
+from keras.layers import Dense, GlobalAveragePooling2D, Dropout
 from keras.models import Model
 from keras.preprocessing.image import ImageDataGenerator
 from keras.applications.mobilenet import preprocess_input
@@ -8,9 +8,9 @@ base_model = MobileNet(weights='imagenet', include_top=False)
 
 bbq_output = base_model.output
 bbq_output = GlobalAveragePooling2D()(bbq_output)
-bbq_output = Dense(1024, activation='relu')(bbq_output)
-bbq_output = Dense(1024, activation='relu')(bbq_output)
 bbq_output = Dense(512, activation='relu')(bbq_output)
+bbq_output = Dense(512, activation='relu')(bbq_output)
+bbq_output = Dropout(0.3)(bbq_output)
 bbq_output = Dense(5, activation='softmax')(bbq_output)
 
 model = Model(inputs=base_model.input, outputs=bbq_output)
@@ -30,19 +30,19 @@ train_generator = train_data_generator.flow_from_directory(
     './images/train',
     target_size=(224, 224),
     color_mode='rgb',
-    batch_size=32,
+    batch_size=16,
     class_mode='categorical',
     shuffle=True
 )
 
-# test_generator = test_data_generator.flow_from_directory(
-#     './images/test',
-#     target_size=(224, 224),
-#     color_mode='rgb',
-#     batch_size=32,
-#     class_mode='categorical',
-#     shuffle=True
-# )
+test_generator = test_data_generator.flow_from_directory(
+    './images/test',
+    target_size=(224, 224),
+    color_mode='rgb',
+    batch_size=16,
+    class_mode='categorical',
+    shuffle=True
+)
 
 model.compile(
     optimizer='Adam',
@@ -55,9 +55,9 @@ step_size_train = train_generator.n//train_generator.batch_size
 model.fit_generator(
     generator=train_generator,
     steps_per_epoch=step_size_train,
-    # validation_data=test_generator,
-    # validation_steps=10,
-    epochs=10
+    validation_data=test_generator,
+    validation_steps=3,
+    epochs=4
 )
 
 model.save('./bbq_model.tf', overwrite=True)
