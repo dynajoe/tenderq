8df4a4a3f306400b0e4b987d54b43298c9640302
Joe Andaverde <joe.andaverde@gmail.com>
2019-03-10 23:31:05 -0500

    predict on all test data'

--- model/predict.py
+++ model/predict.py
@@ -1,17 +1,30 @@
 from keras.models import load_model
 from keras.applications.mobilenet import preprocess_input
 from keras.preprocessing.image import img_to_array, load_img
+from keras.preprocessing.image import ImageDataGenerator
+from keras.applications.mobilenet import preprocess_input
+import numpy as np
 
-bbq_model = load_model('./bbq_model.h5')
+model = load_model('./bbq_model.h5')
 
-img = preprocess_input(img_to_array(
-    load_img('./images/test/brisket/shawnee_brisket.jpg')))
+test_data_generator = ImageDataGenerator(
+    preprocessing_function=preprocess_input
+)
 
-formatted_image = img.reshape((1,) + img.shape)
+test_generator = test_data_generator.flow_from_directory(
+    './images/test',
+    target_size=(224, 224),
+    color_mode='rgb',
+    batch_size=1,
+    class_mode='categorical'
+)
 
-probabilities = bbq_model.predict(formatted_image)
+labels = dict((v,k) for k,v in test_generator.class_indices.items())
 
-labels = sorted(['ribs', 'chicken', 'brisket', 'pork', 'other'])
-
-print(probabilities, labels)
-print('prediction: ' + labels[probabilities.argmax(axis=-1)[0]])
+for i in range(len(test_generator)):
+    (img, category) = test_generator[i]
+    probabilities = model.predict(img)
+    label = labels[category.argmax(axis=-1)[0]]
+    prediction = labels[probabilities.argmax(axis=-1)[0]]
+    print(("FAIL", "OK")[label == prediction], ' label:', label, 'prediction: ', prediction)
+    
