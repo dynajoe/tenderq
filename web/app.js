af0e88cfb175b5e4f78d1f14d1cd44a6bf0b488e
Joe Andaverde <joe.andaverde@gmail.com>
2019-03-11 23:49:37 -0500

    preference

--- web/app.js
+++ web/app.js
@@ -1,32 +1,32 @@
-var createError = require('http-errors')
-var express = require('express')
-var path = require('path')
-var cookieParser = require('cookie-parser')
-var logger = require('morgan')
+const CreateError = require('http-errors')
+const Express = require('express')
+const Path = require('path')
+const CookieParser = require('cookie-parser')
+const Logger = require('morgan')
 
-var indexRouter = require('./routes/index')
+const indexRouter = require('./routes/index')
 
-var app = express()
+const app = Express()
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', Path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
-app.use(logger('dev'))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(Logger('dev'))
+app.use(Express.json())
+app.use(Express.urlencoded({ extended: false }))
+app.use(CookieParser())
+app.use(Express.static(Path.join(__dirname, 'public')))
 
 app.use('/', indexRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-   next(createError(404))
+app.use((req, res, next) => {
+   next(CreateError(404))
 })
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use((err, req, res, next) => {
    // set locals, only providing error in development
    res.locals.message = err.message
    res.locals.error = req.app.get('env') === 'development' ? err : {}
